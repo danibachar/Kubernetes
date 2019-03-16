0738b39509740114a3ef12ab046e8e16430ed981
I <danibachar89@gmail.com>
2019-03-17 01:21:54 +0200

    update

--- yoyo_attacker/yoyo_attaker_flow.py
+++ yoyo_attacker/yoyo_attaker_flow.py
@@ -298,7 +298,7 @@ def start():
     We
     """
 
-    config.load_kube_config()
+    # config.load_kube_config()
     api_instance = client.AutoscalingV1Api()
 
 
@@ -458,29 +458,29 @@ def start():
     print('config - {}'.format(CONFIG))
 #loadtest http://35.226.116.34:31001/service -t 60 -c 10 --rps 4
 # start_yoyo_attack()
-start()
+# start()
 
-# from google.auth import compute_engine
-# from google.cloud.container_v1 import ClusterManagerClient
-# from kubernetes import client
-#
-# project_id = 'woven-phoenix-234610'
-# zone = 'us-central1-a'
-# cluster_id = 'yoyo-attack'
-#
-# credentials = compute_engine.Credentials()
-#
-# cluster_manager_client = ClusterManagerClient(credentials=credentials)
-# cluster = cluster_manager_client.get_cluster(project_id, zone, cluster_id)
-#
-# configuration = client.Configuration()
-# configuration.host = f"https://{cluster.endpoint}:443"
-# configuration.verify_ssl = False
-# configuration.api_key = {"authorization": "Bearer " + credentials.token}
-# client.Configuration.set_default(configuration)
-#
-# v1 = client.CoreV1Api()
-# print("Listing pods with their IPs:")
-# pods = v1.list_pod_for_all_namespaces(watch=False)
-# for i in pods.items:
-#     print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
+from google.auth import compute_engine
+from google.cloud.container_v1 import ClusterManagerClient
+from kubernetes import client
+
+project_id = 'woven-phoenix-234610'
+zone = 'us-central1-a'
+cluster_id = 'yoyo-attack'
+
+credentials = compute_engine.Credentials()
+
+cluster_manager_client = ClusterManagerClient(credentials=credentials)
+cluster = cluster_manager_client.get_cluster(project_id, zone, cluster_id)
+
+configuration = client.Configuration()
+configuration.host = f"https://{cluster.endpoint}:443"
+configuration.verify_ssl = False
+configuration.api_key = {"authorization": "Bearer " + credentials.token}
+client.Configuration.set_default(configuration)
+
+v1 = client.CoreV1Api()
+print("Listing pods with their IPs:")
+pods = v1.list_pod_for_all_namespaces(watch=False)
+for i in pods.items:
+    print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
