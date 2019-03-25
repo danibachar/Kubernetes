d3e0e3b168ed489d7f26ad767ab4a133910dcd1e
I <danibachar89@gmail.com>
2019-03-25 23:39:14 +0200

    update power of attack

--- yoyo_attacker/yoyo_attaker_flow.py
+++ yoyo_attacker/yoyo_attaker_flow.py
@@ -32,7 +32,7 @@ END_POINT = 'http://35.238.129.124:31001/service/1000'
 CONFIG = {
     'scaled_attack': False,  # A new options - aas noticed in experiments
     'r': 5,  # Average requests rate per unit time of legitimate clients
-    'k': 4,  # power of attack
+    'k': 10,  # power of attack
     'n': 0,  # Number of attack cycles - Should be dynamic counter every on attack
     't': 0,  # Cycle duration in seconds
     't_on': 80,
