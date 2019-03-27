016f6362a11f4faf37cb60573cc15654d255fa2a
I <danibachar89@gmail.com>
2019-03-28 01:48:33 +0200

    ip update

--- yoyo_attacker/yoyo_attaker_flow.py
+++ yoyo_attacker/yoyo_attaker_flow.py
@@ -28,7 +28,7 @@ def safe_open(file_name_with_dierctory: str, permision="wb+"):
 
 
 # GLOBALS
-END_POINT = 'http://35.184.105.95:31001/service/1000'
+END_POINT = 'http://35.239.228.131:31001/service/1000'
 CONFIG = {
     'scaled_attack': False,  # A new options - aas noticed in experiments
     'r': 5,  # Average requests rate per unit time of legitimate clients
